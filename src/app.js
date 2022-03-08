5af7bd461dd3cf7715b1663eff23747c374fcd24
danyez1 <cdanielyanez144@gmail.com>
2022-03-08 01:59:29 +0000

    finis

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 /* eslint-disable */
+
+import "bootstrap";
+import "./style.css";
 const suits = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
 const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
 //write your code here
 
 function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  const float = Math.random() * (max - min) + min;
+  return Math.floor(float);
 }
 
 let suitsIndex = getRandomNumber(0, suits.length - 1);
 let numbersIndex = getRandomNumber(0, numbers.length - 1);
 
 window.onload = function() {
-  let cardn = document.querySelector("#card");
-  cardn.innerHTML = numbersIndex;
+  let cardrandom = document.querySelector(".suits.top");
+  cardrandom.innerHTML = suits[suitsIndex];
+
+  let numberRandom = document.querySelector(".number");
+  numberRandom.innerHTML = numbers[numbersIndex];
+
+  let suitsRandom = document.querySelector(".suits.bottom");
+  suitsRandom.innerHTML = suits[suitsIndex];
 };
